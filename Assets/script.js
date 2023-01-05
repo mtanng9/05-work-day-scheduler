452d9d1d2d54573850b46b576d255d27912b0709
mtanng9 <mlt152498@gmail.com>
2023-01-04 23:22:45 -0500

    added timeblocks along with logic to color code them based on current time and save and load tasks

--- Assets/script.js
+++ Assets/script.js
@@ -9,15 +9,63 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var saveButtons = $('.saveBtn');
+  for (var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener("click", function() {
+      var value = this.parentElement.children[1].value;
+      if (value.trim() != '') {
+        window.localStorage.setItem(this.parentElement.id, value);
+      }
+    })
+  }
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var timeBlocks = $('.time-block');
+
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var timeBlock = timeBlocks[i];
+    var timeBlockString = timeBlock.firstElementChild.textContent;
+    var timeBlockHour =  parseInt(timeBlockString.slice(0,timeBlockString.length - 2));
+    var timeBlockConvention = timeBlockString.slice(timeBlockString.length - 2, timeBlockString.length);
+
+    if (timeBlockConvention === "PM" && timeBlockHour != 12) {
+        timeBlockHour += 12;
+    }
+
+    var timeBlockTime = dayjs().hour(timeBlockHour);
+    timeBlockTime = timeBlockTime.minute(00);
+    timeBlockTime = timeBlockTime.second(00);
+
+    var topOfTheHour = dayjs();
+    topOfTheHour = topOfTheHour.hour(00);
+    topOfTheHour = topOfTheHour.minute(00);
+
+    if (timeBlockTime.isSame(topOfTheHour)) {
+      timeBlock.className = timeBlock.className + " present";
+    } else if (timeBlockTime.isBefore(topOfTheHour)) {
+      timeBlock.className = timeBlock.className + " future";
+    } else if (timeBlockTime.isAfter(topOfTheHour)){
+      timeBlock.className = timeBlock.className + " past";
+    }
+  }
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var timeBlock = timeBlocks[i];
+    var storedValue = window.localStorage.getItem(timeBlock.id);
+    if (storedValue != null) {
+      timeBlock.children[1].value = storedValue;
+    }
+  }
+
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('MMM D, YYYY'); 
+  $('#currentDay').text(currentDate);
 });
